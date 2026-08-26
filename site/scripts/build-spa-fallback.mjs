098f6e7dae23d0dfd144ffe82261a82fc38310ef
Dawid Skinder <kontakt@dawidskinder.pl>
2026-08-26 17:51:19 +0200

    Keep hash routing canonical on Pages

--- site/scripts/build-spa-fallback.mjs
+++
@@ -1,9 +0,0 @@
-import { copyFile } from "node:fs/promises";
-import { fileURLToPath } from "node:url";
-
-const outputDirectory = new URL("../dist/", import.meta.url);
-
-await copyFile(
-  fileURLToPath(new URL("index.html", outputDirectory)),
-  fileURLToPath(new URL("404.html", outputDirectory)),
-);
